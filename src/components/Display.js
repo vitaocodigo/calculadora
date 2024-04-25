54b6fd46b4f3c6586c8c6a57ec1d05bd47685207
vitaocodigo <victor.mishima@etec.sp.gov.br>
2024-04-25 08:40:33 -0300

    criado componente display.js

---
+++ src/components/Display.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Dimensions, Stylesheet, Text, View } from "react-native";
+
+const styles = Stylesheet.create({
+    display: {
+        flex: 1,
+        padding: 20 ,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0,0,)',
+        alignitems: 'flex-end'
+    },
+    displayValue: {
+        fontSize: 60,
+        color: '#ffffff'
+    }
+})
